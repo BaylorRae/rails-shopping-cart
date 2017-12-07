6cb758884a4426ae3c63d2fb69f7fb8c9c804b86
Baylor Rae <baylor@thecodedeli.com>
2017-12-06 21:25:44 -0600

    add associations

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,5 @@
 class Order < ApplicationRecord
+
+  has_many :items, class_name: 'OrderItem'
+
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,6 @@ class Product < ApplicationRecord
   has_many :product_categories
   has_many :categories, through: :product_categories
 
+  has_many :variants, class_name: 'ProductVariant'
+
 end
